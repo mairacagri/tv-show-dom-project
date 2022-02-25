8168b30344ac1ce3f17e8060833a401aac882182
mairacagri <mairacagri@gmail.com>
2022-02-25 20:13:59 +0000

    script changes

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 //You can edit ALL of the code here
 function setup() {
   const allEpisodes = getAllEpisodes();
-  console.log(allEpisodes);
+  // console.log(allEpisodes);
   // let card_box
   makePageForEpisodes(allEpisodes);
 }
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-
-  episodeList.forEach( (episode) => {
+    
+  return episodeList.forEach( (episode) => {
     let content = document.createElement("div");
     content.setAttribute("id", "content")
+    let divTitle = document.createElement("div");
+    divTitle.className = "div-title";
     let nameEl = document.createElement("div-name");//creating div for name of episode
     // nameEl.classList.add("name");
     nameEl.className = "name"
@@ -21,20 +23,40 @@ function makePageForEpisodes(episodeList) {
     let episodeNumEl = document.createElement("div-episode-number");//creating  div for episode number
     episodeNumEl.className = "episode-number"
     let imgEl = document.createElement("img");
-    imgEl.src = ""; //need to find a way to add src
-    imgEl.alt = "photo of awesome Game Of Thrones " 
+    imgEl.setAttribute("src", episode.image.medium);
+    // imgEl.src = ""; //need to find a way to add src
+    // imgEl.alt = "photo of awesome Game Of Thrones " 
     let summaryTextEl = document.createElement("p"); //creating p for summary...but its already exist there
     summaryTextEl.innerHTML = episode.summary; // kinda replacing existed p 
 
-    content.appendChild(nameEl);
-    content.appendChild(seasonNumEl);
-    content.appendChild(episodeNumEl);
+
+    let twoDigit =(number) => {
+        if(number < 10){
+          return 0 + number
+        }else {
+          return number
+        }
+    }
+    divTitle.innerHTML = `${episode.name} - ${twoDigit(episode.number)}` 
+
+    divTitle.appendChild(nameEl);
+    divTitle.appendChild(seasonNumEl);
+    divTitle.appendChild(episodeNumEl);
+    divTitle.appendChild(summaryTextEl);
+    content.appendChild(divTitle);
     content.appendChild(imgEl);
-    content.appendChild(summaryTextEl)
+    rootElem.appendChild(content);
+    console.log(content);
+
+
+    // content.appendChild(nameEl);
+    // content.appendChild(seasonNumEl);
+    // content.appendChild(episodeNumEl);
+    // content.appendChild(summaryTextEl)
 
   });
 
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  // rootElem.textContent = `Got ${episodeList.length} episode(s)`;
 }
 
 window.onload = setup;
