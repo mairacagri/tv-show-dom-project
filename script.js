dfbb1c59e136ca34655d4e5dc2c7cd4497c74df4
mairacagri <mairacagri@gmail.com>
2022-02-25 21:20:12 +0000

    script and little bit of css

--- script.js
+++ script.js
@@ -12,32 +12,38 @@ function makePageForEpisodes(episodeList) {
   return episodeList.forEach( (episode) => {
     let content = document.createElement("div");
     content.setAttribute("id", "content")
+
     let divTitle = document.createElement("div");
     divTitle.className = "div-title";
+
     let nameEl = document.createElement("div-name");//creating div for name of episode
     // nameEl.classList.add("name");
-    nameEl.className = "name"
+    nameEl.className = "name";
+
     let seasonNumEl = document.createElement("div-season-number"); //creating div for season number
     // seasonNumEl.classList.add("seasonEl")
-    seasonNumEl.className = "season-number"
+    seasonNumEl.className = "season-number";
+
     let episodeNumEl = document.createElement("div-episode-number");//creating  div for episode number
     episodeNumEl.className = "episode-number"
+
     let imgEl = document.createElement("img");
     imgEl.setAttribute("src", episode.image.medium);
     // imgEl.src = ""; //need to find a way to add src
     // imgEl.alt = "photo of awesome Game Of Thrones " 
     let summaryTextEl = document.createElement("p"); //creating p for summary...but its already exist there
+    summaryTextEl.className = "summary";
     summaryTextEl.innerHTML = episode.summary; // kinda replacing existed p 
 
-
+    //adding zero for seasons and  episodes under 10
     let twoDigit =(number) => {
         if(number < 10){
-          return 0 + number
+          return `0${number}`;
         }else {
-          return number
+          return number;
         }
     }
-    divTitle.innerHTML = `${episode.name} - ${twoDigit(episode.number)}` 
+    divTitle.innerHTML = `${episode.name} - S${twoDigit(episode.season)}E${twoDigit(episode.number)}` 
 
     divTitle.appendChild(nameEl);
     divTitle.appendChild(seasonNumEl);
