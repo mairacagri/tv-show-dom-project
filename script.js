be89bb27e56873c7a9d3c697a5d1088287d35c51
mairacagri <mairacagri@gmail.com>
2022-02-25 22:48:15 +0000

    fixed season and episode

--- script.js
+++ script.js
@@ -13,52 +13,49 @@ function makePageForEpisodes(episodeList) {
     let content = document.createElement("div");
     content.setAttribute("id", "content")
 
-    let divTitle = document.createElement("div");
-    divTitle.className = "div-title";
-
-    let nameEl = document.createElement("div-name");//creating div for name of episode
-    // nameEl.classList.add("name");
-    nameEl.className = "name";
-
-    let seasonNumEl = document.createElement("div-season-number"); //creating div for season number
-    // seasonNumEl.classList.add("seasonEl")
-    seasonNumEl.className = "season-number";
-
-    let episodeNumEl = document.createElement("div-episode-number");//creating  div for episode number
-    episodeNumEl.className = "episode-number"
-
-    let imgEl = document.createElement("img");
-    imgEl.setAttribute("src", episode.image.medium);
-    // imgEl.src = ""; //need to find a way to add src
-    // imgEl.alt = "photo of awesome Game Of Thrones " 
-    let summaryTextEl = document.createElement("p"); //creating p for summary...but its already exist there
-    summaryTextEl.className = "summary";
-    summaryTextEl.innerHTML = episode.summary; // kinda replacing existed p 
-
-    //adding zero for seasons and  episodes under 10
-    let twoDigit =(number) => {
+      //adding zero for seasons and  episodes under 10
+      let twoDigit =(number) => {
         if(number < 10){
           return `0${number}`;
         }else {
           return number;
         }
     }
-    divTitle.innerHTML = `${episode.name} - S${twoDigit(episode.season)}E${twoDigit(episode.number)}` 
+    
+    //creating div for name of episode
+    let nameEl = document.createElement("div-name");
+    nameEl.className = "name";
+    nameEl.innerHTML = `${episode.name} S${twoDigit(episode.season)}E${twoDigit(episode.number)}`;
 
-    divTitle.appendChild(nameEl);
-    divTitle.appendChild(seasonNumEl);
-    divTitle.appendChild(episodeNumEl);
-    divTitle.appendChild(summaryTextEl);
-    content.appendChild(divTitle);
-    content.appendChild(imgEl);
+    //creating div for season number
+    let seasonNumEl = document.createElement("div-season-number"); 
+    // seasonNumEl.classList.add("seasonEl")
+    // seasonNumEl.className = "season-number";
+
+    //creating  div for episode number
+    let episodeNumEl = document.createElement("div-episode-number");
+    // episodeNumEl.className = "episode-number"
+    let imgDiv = document.createElement("div");
+    imgDiv.className = "image";
+    let imgEl = document.createElement("img");
+    imgEl.setAttribute("src", episode.image.medium);
+   
+    //creating p for summary...but its already exist there
+    let summaryTextEl = document.createElement("p"); 
+    summaryTextEl.className = "summary";
+    // kinda replacing existed p 
+    summaryTextEl.innerHTML = episode.summary; 
+
+   
+
+    content.appendChild(nameEl);
+    content.appendChild(seasonNumEl);
+    content.appendChild(episodeNumEl);
+    content.appendChild(summaryTextEl);
+    content.appendChild(imgDiv);
+    imgDiv.appendChild(imgEl);
     rootElem.appendChild(content);
     console.log(content);
-
-
-    // content.appendChild(nameEl);
-    // content.appendChild(seasonNumEl);
-    // content.appendChild(episodeNumEl);
-    // content.appendChild(summaryTextEl)
 
   });
 
